dcfc5ce53d05ab88d6ef0ac4ebe5a960d1c3fa25
WAKISTA Chamith <chamith.wakista@wfp.org>
2023-08-03 13:11:26 +0530

    Logo added

--- App.js
+++ App.js
@@ -4,19 +4,24 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import Scanner from './screens/Scanner';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
-
 import { NavigationContainer } from '@react-navigation/native';
+import Logo from './assets/logo'; // Replace with the actual path to your Logo component
 
 const Stack = createStackNavigator();
 
-
-
 function App() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Scanner" component={Scanner} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Stack.Navigator
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Scanner" component={Scanner} />
+      </Stack.Navigator>
+      {/* Logo after the header */}
+      <View style={styles.logoContainer}>
+        <Logo />
+      </View>
+    </View>
   );
 }
 
@@ -24,17 +29,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'right',
     justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center', // Center the logo horizontally
+    marginTop: 10, // Add some margin to separate the logo from the header
   },
 });
 
 export default () => {
   return (
     <NavigationContainer>
-     
-        <App />
-      
+      <App />
     </NavigationContainer>
-  )
-}
+  );
+};
--- screens/Home.js
+++ screens/Home.js
@@ -20,8 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'right',
+    justifyContent: 'right',
     
   },
 });
